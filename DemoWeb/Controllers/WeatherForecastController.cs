41a9fa4313cf4e7db6c58d56908597932fec5c58
zhengqiangzheng <42822794+zhengqiangzheng@users.noreply.github.com>
2020-07-16 00:19:37 +0800

    Add files via upload

---
+++ DemoWeb/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Data;
+using Demo.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DemoWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DemoContext context;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,DemoContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Club> Get()
+        {
+            var clubs = context.Club.ToList();
+            return clubs;
+
+        }
+    }
+}
