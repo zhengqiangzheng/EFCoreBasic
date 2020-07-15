8afab578947841192d4371209c8329a13650f4ae
zhengqiangzheng <2466877980@qq.com>
2020-07-16 00:00:40 +0800

    first push

---
+++ Club.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Demo.Domain
+{
+    public class Club
+    {
+
+        public Club()
+        {
+            Players = new List<Player>();
+        }
+        public List<Player> Players { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        [Column(TypeName ="date")]
+        public DateTime DateOfEstablishment { get; set; }
+        public string History { get; set; }
+        public League League { get; set; }
+
+
+    }
+}
---
+++ League.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Domain
+{
+    public class League
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required,MaxLength(50)]
+        public string Country { get; set; }
+    }
+}
